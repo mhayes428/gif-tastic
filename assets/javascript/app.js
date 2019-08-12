58ee7b5120202545beae40ccf24751627640937f
Mike Hayes <fikes428@gmail.com>
2019-08-12 12:00:10 -0400

    create array and document ready

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,7 @@
 var characterArray = ["Aragorn", "Legolas", "Gandalf", "Gimli"];
+
+$(document).ready(function () {
+    for (var i = 0; i < characterArray.length; i++) {
+        $("#character-buttons").append("button type='button' onClick='searchGif(/"" + characterArray[i] + " / ")' class='btn btn-primary' value=' " + characterArray[i] + " </button>"); 
+    }
+})
