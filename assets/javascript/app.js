bf85dac91e9aa182dd054146acbf14d3af6fd8c4
Mike Hayes <fikes428@gmail.com>
2019-08-12 12:04:34 -0400

    js punctuation fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,6 @@ var characterArray = ["Aragorn", "Legolas", "Gandalf", "Gimli"];
 
 $(document).ready(function () {
     for (var i = 0; i < characterArray.length; i++) {
-        $("#character-buttons").append("button type='button' onClick='searchGif(/"" + characterArray[i] + " / ")' class='btn btn-primary' value=' " + characterArray[i] + " </button>"); 
-    }
-})
+        $("#character-buttons").append("<button type='button' onclick='searchGif(\"" + characterArray[i] + "\")' class='btn btn-primary' value=' " + characterArray[i] + "'> " + characterArray[i] + " </button>");
+    } console.log("this works")
+});
