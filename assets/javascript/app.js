74bab7e321eb80dbf6c0ea8ca5c2d31c3082f249
Mike Hayes <fikes428@gmail.com>
2019-08-12 12:08:49 -0400

    Add char button/submit button function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,5 +3,18 @@ var characterArray = ["Aragorn", "Legolas", "Gandalf", "Gimli"];
 $(document).ready(function () {
     for (var i = 0; i < characterArray.length; i++) {
         $("#character-buttons").append("<button type='button' onclick='searchGif(\"" + characterArray[i] + "\")' class='btn btn-primary' value=' " + characterArray[i] + "'> " + characterArray[i] + " </button>");
-    } console.log("this works")
+    } //console.log("this works")
 });
+
+function characterButtonClicked() {
+    var userInput = $('#character-input').val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked() {
+    var userInput = $('#character-input').val();
+
+    if (userInput) {
+        $('#character-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
